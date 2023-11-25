6569003ae72f51d7db3a32964aad21f7012ab5fb
sqzw-x <mdcx.feedback@gmail.com>
2023-11-25 22:34:41 +0800

    Fix: 非整数倍缩放 (#5)

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sys
 
 import urllib3  # yapf: disable # NOQA: E402
 from PIL import ImageFile
-from PyQt5.QtCore import Qt, QCoreApplication
+from PyQt5.QtCore import QCoreApplication, Qt
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 
@@ -33,10 +33,11 @@ if __name__ == '__main__':
                 info["LSUIElement"] = True
             except:
                 pass
-        if os.path.isfile('highdpi_passthrough'):
-            # 解决不同电脑不同缩放比例问题，非整数倍缩放，如系统中设置了150%的缩放，QT程序的缩放将是两倍，QT 5.14中增加了非整数倍的支持，需要加入下面的代码才能使用150%的缩放
-            # 默认是 Qt.HighDpiScaleFactorRoundingPolicy.Round，会将150%缩放变成200%
-            QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+
+    if os.path.isfile('highdpi_passthrough'):
+        # 解决不同电脑不同缩放比例问题，非整数倍缩放，如系统中设置了150%的缩放，QT程序的缩放将是两倍，QT 5.14中增加了非整数倍的支持，需要加入下面的代码才能使用150%的缩放
+        # 默认是 Qt.HighDpiScaleFactorRoundingPolicy.Round，会将150%缩放变成200%
+        QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
 
     # 适应高DPI设备
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
