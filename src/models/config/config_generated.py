b6b8e860ea8a3a3e2e3498836a091a3a017327f2
sqzw-x <mdcx.feedback@gmail.com>
2024-01-19 15:43:18 +0800

    Ver 120240119

--- src/models/config/config_generated.py
+++ src/models/config/config_generated.py
@@ -10,8 +10,8 @@ class GeneratedConfig:
     Also used as default configuration
     """
     # modified_time
-    modified_time = r'2024-01-13 14:36:53'
-    version = 120240113
+    modified_time = r'2024-01-19 15:41:33'
+    version = 120240119
 
     # media
     media_path = r''
