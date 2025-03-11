b988df6a85d62caff3741306eba17cdc6e90a38d
sqzw-x <mdcx.feedback@gmail.com>
2025-03-12 00:50:25 +0800

    feat: 添加 nuitka 构建参数隐藏控制台

--- build-nuitka.ps1
+++ build-nuitka.ps1
@@ -1,5 +1,6 @@
 $env:PYTHONPATH = "$PWD\src"
 
+# 体积可减小至 ~44MB, 运行速度无明显变化. 构建耗时较长.
 $nuitkaArgs = @(
     '--standalone' # 包含 Python
     '--onefile'
@@ -8,6 +9,7 @@ $nuitkaArgs = @(
     '--include-module=_distutils_hack'
     '--include-data-dir=resources=resources'
     '--enable-plugin=pyqt5'
+    '--windows-disable-console'
     '.\main.py'
 )
 
