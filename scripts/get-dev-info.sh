a9d8239b5f377cac5e3bb9b3b51d5c9deb9a94b1
sqzw <mdcx.feedback@gmail.com>
2025-06-27 14:17:58 +0800

    CI: use external scripts

---
+++ scripts/get-dev-info.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 获取最后一个commit的信息
+echo 'LAST_COMMIT<<END_LAST_COMMIT'
+git log -1 --pretty=format:"**Latest Commit:** %s%n%nAuthor: %an%nDate: %ai%nSHA: %H%n"
+echo 'END_LAST_COMMIT'
+
+# 获取最后一个tag
+LAST_TAG=$(git describe --tags --abbrev=0 2>/dev/null || echo "No tags found")
+echo "last_tag=$LAST_TAG"
+
+# 获取自最后一个tag以来的commit历史
+echo 'COMMITS_SINCE_TAG<<END_COMMITS_SINCE_TAG'
+if [ "$LAST_TAG" != "No tags found" ]; then
+    echo "**Commits since $LAST_TAG:**"
+    echo ""
+    git log $LAST_TAG..HEAD --pretty=format:"- %s (%an, %ar)" --reverse
+else
+    echo "**All commits:**"
+    echo ""
+    git log --pretty=format:"- %s (%an, %ar)" --reverse
+fi
+echo ''
+echo 'END_COMMITS_SINCE_TAG'
